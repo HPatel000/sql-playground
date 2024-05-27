5d80126acc0e004438e0ab961f621ab71ca07d2b
HPatel000 <patelheli1711@gmail.com>
2024-05-27 22:52:59 +0530

    basic set up for electron nextjs and access backend functionality with preload

---
+++ main/main.js
@@ -0,0 +1,59 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+// const serve = require('electron-serve');
+const path = require('path');
+const { Sequelize } = require('sequelize');
+
+// const appServe = app.isPackaged
+//   ? serve({
+//       directory: path.join(__dirname, '../out'),
+//     })
+//   : null;
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // if (app.isPackaged) {
+  //   appServe(win).then(() => {
+  //     win.loadURL('app://-');
+  //   });
+  // } else {
+  win.loadURL('http://localhost:3000');
+  win.webContents.openDevTools();
+  win.webContents.on('did-fail-load', (e, code, desc) => {
+    win.webContents.reloadIgnoringCache();
+  });
+  // }
+
+  ipcMain.handle('execute-sql', async (event, query) => {
+    const sequelize = new Sequelize('world', 'root', 'root', {
+      dialect: 'mysql',
+      host: 'localhost',
+    });
+
+    return new Promise((resolve, reject) => {
+      sequelize.query(query, (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+        sequelize.end();
+      });
+    });
+  });
+};
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
